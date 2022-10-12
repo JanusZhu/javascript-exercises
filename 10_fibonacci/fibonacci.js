a61f76d2e31394c075e3e71deef7b2f3b2c3be12
JanusZhu <janus.zhu@yale.edu>
2022-10-12 18:18:54 +0900

    added solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  if (isNaN(a)) {
+    return false;
+  }
+  a = parseInt(a);
+  if (a <= 0) {
+    return "OOPS";
+  }
+  if (a == 1) {
+    return 1;
+  }
+  if (a == 2) {
+    return 1;
+  }
+  return fibonacci(a - 1) + fibonacci(a - 2);
 };
 
 // Do not edit below this line
