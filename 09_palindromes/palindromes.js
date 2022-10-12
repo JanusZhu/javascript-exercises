48458ff78ef9e508ae3ce46e731d3466b5cc477e
JanusZhu <janus.zhu@yale.edu>
2022-10-12 18:09:27 +0900

    added solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  //console.log(word);
+  if (word.length == 0) {
+    return true;
+  }
+  if (word.length == 1) {
+    return true;
+  }
+  let l = 0;
+  let r = word.length - 1;
+  //get ride of non-letters
+  while (word[l].toLowerCase() == word[l].toUpperCase()) {
+    l += 1;
+  }
+  while (word[r].toLowerCase() == word[r].toUpperCase()) {
+    r -= 1;
+  }
+  return word[l].toLowerCase() == word[r].toLowerCase()
+    ? palindromes(word.slice(l + 1, r))
+    : false;
 };
+
+//console.log(palindromes("Animal loots foliated detail of stool lamina."));
 
 // Do not edit below this line
 module.exports = palindromes;
